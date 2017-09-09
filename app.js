6d3598644ce532c755fa782feadff7ba39cba74a
astigmatism <eric@crazyerics.com>
2017-09-08 17:07:08 -0700

    cors on static files

--- app.js
+++ app.js
@@ -20,15 +20,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Add headers
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: (res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  }
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
