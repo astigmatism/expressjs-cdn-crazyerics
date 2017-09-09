8d9d37970ba713d8b00b78d2661be3d06c99cde8
astigmatism <eric@crazyerics.com>
2017-09-08 22:41:09 -0700

    beginings

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// var index = require('./routes/index');
-// var users = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -27,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public'), {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
   }
 }));
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
