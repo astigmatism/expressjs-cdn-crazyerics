60bb71a707ce3d2771c79e42eb1ed912a6b1f432
astigmatism <eric@crazyerics.com>
2018-01-05 21:39:27 -0800

    mispelling

--- app.js
+++ app.js
@@ -26,17 +26,17 @@ app.use(express.static('public'));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
 
   // Request headers you wish to allow
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
+  //res.header('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
