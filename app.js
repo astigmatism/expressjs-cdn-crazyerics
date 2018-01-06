3a62c66954fc6ad142031e974d07a4bc26c7edf0
astigmatism <eric@crazyerics.com>
2018-01-05 21:43:18 -0800

    back

--- app.js
+++ app.js
@@ -20,35 +20,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static('public'));
-
 //CORS
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-
-  // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  //res.header('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-// app.use(express.static(path.join(__dirname, 'public'), {
-//   setHeaders: (res) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,POST');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   }
-// }));
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: (res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  }
+}));
 
 app.use('/', routes);
 
