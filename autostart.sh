e0152a1c043f46ec5c71bcc77106fafbe5f69890
astigmatism <eric@crazyerics.com>
2018-08-21 15:38:40 -0700

    updates

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,6 @@
 # Here's an autostart script file!
 # requires pm2. pathing is specific for the location of the app.js file
+# for some reason, pm2 works when in the working directory, otherwise it sucks
 # use cron for start up: "crontab -e" and add "@reboot /path/to/script"
 
 cd /media/data/expressjs-cdn-crazyerics/
