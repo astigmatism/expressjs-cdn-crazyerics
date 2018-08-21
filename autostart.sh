3565575d5d4c1e822832e54fab67fbed57eebbee
astigmatism <eric@crazyerics.com>
2018-08-21 15:36:26 -0700

    updates

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,6 @@
-pm2 start /media/data/expressjs-cdn-crazyerics/app.js --name cdn -i -1
+# Here's an autostart script file!
+# requires pm2. pathing is specific for the location of the app.js file
+# use cron for start up: "crontab -e" and add "@reboot /path/to/script"
+
+cd /media/data/expressjs-cdn-crazyerics/
+pm2 start app.js --name cdn -i -1
